347c05a0c1f507f4fae0c616a82e3a6eefcf2b09
Benjamin Silva <benjamin.silva@insight4.tech>
2023-03-07 00:15:01 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001
+const axios = require('axios');
+const circuitBreaker = require('opossum');
+
+const circuitBreakerOptions = {
+    errorThresholdPercentage: 50,
+    resetTimeout: 5000,
+    rollingCountTimeout: 10000,
+    fallback: () => 'Service unavailable. Please try again later.'
+};
+
+const apiCall = () => axios.get('smartree-api.azurewebsites.net/api/dashboard');
+
+const circuitBreakerInstance = new circuitBreaker(apiCall, circuitBreakerOptions);
+
+app.get('/api/circuit', async (req, res) => {
+    try {
+        const responseBody = await circuitBreakerInstance.fire();
+        res.json(responseBody.data);
+    } catch (error) {
+        res.json(error.message);
+    }    
+});
+
+app.listen( port, () => {
+  console.log(`Server running on port ${port}`);
+});
