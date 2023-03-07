2e1734f8d932e0d8d7a64730e0f4c48e7afd7f84
Benjamin Silva <benjamin.silva@insight4.tech>
2023-03-07 06:58:44 -0500

    add slow service

--- app.js
+++ app.js
@@ -8,16 +8,38 @@ const circuitBreakerOptions = {
     errorThresholdPercentage: 50,
     resetTimeout: 5000,
     rollingCountTimeout: 10000,
+    setTimeout: 3000,
     fallback: () => 'Service unavailable. Please try again later.'
 };
 
 const apiCall = () => axios.get('https://smartree-api.azurewebsites.net:443/api/dashboard');
+const apiCall2 = () => axios.get('https://smartree-api.azurewebsites.net:443/api/slow-service?timeout=${timeout}');
 
 const circuitBreakerInstance = new circuitBreaker(apiCall, circuitBreakerOptions);
+const circuitBreakerSlow = new circuitBreaker(apiCall2, circuitBreakerOptions);
 
 app.get('/api/src', async (req, res) => {
     try {
         const responseBody = await circuitBreakerInstance.fire();
+        res.json(responseBody.data);
+    } catch (error) {
+        res.json(error.message);
+    }    
+});
+
+app.get('/api/src', async (req, res) => {
+    try {
+        const responseBody = await circuitBreakerInstance.fire();
+        res.json(responseBody.data);
+    } catch (error) {
+        res.json(error.message);
+    }    
+});
+
+app.get('/api/slow-service', async (req, res) => {
+    try {
+        circuitBreakerSlow
+        const responseBody = await circuitBreakerSlow.fire(req.query.timeout);
         res.json(responseBody.data);
     } catch (error) {
         res.json(error.message);
